dba6060b540015918b5935614d0dcdef2956bf68
danielclarksclass <danielneugent@amdg.rockhursths.edu>
2021-09-17 10:57:07 -0500

    Init

---
+++ src/Webdriver.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Webdriver {
+    public static void startTab() {
+        System.out.println("placeholder");
+        // locating where the webdriver is
+        System.setProperty("webdriver.chrome.driver", "")
+    }
+}
