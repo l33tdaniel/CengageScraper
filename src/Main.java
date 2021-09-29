48959509d1a654599e3630a56e634961e76753a2
Daniel Neugent <danielneugent@Daniels-iMac.local>
2021-09-28 18:02:04 -0700

    turned 'Webdriver into 'WebDriver'

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,7 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        /*
-        Webdriver driver = new Webdriver();
-        driver.startTab();
-        */
         String CengageLink = "https://nglsync.cengage.com/portal/Account/LogOn?ReturnUrl=%2fportal";
         CengageDriver Cengage = new CengageDriver(CengageLink);
-        
-        
+
     }
 }
